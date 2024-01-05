c2cca0c4b9e11244419102f36640c906da348cae
taeseongHan <taeseong0505@naver.com>
2024-01-05 14:30:33 +0900

    [refactor] add up method

--- Assets/Scripts/Monster/MonsterBullet.cs
+++ Assets/Scripts/Monster/MonsterBullet.cs
@@ -13,8 +13,8 @@ public class MonsterBullet : MonoBehaviour
 
     private Transform _monsterTransform;
     private Transform _playerTransform;
-        
-    
+   
+
     private void Start()
     {
       GameManager.Difficulty difficulty = GameManager.Instance.difficulty;
@@ -27,30 +27,19 @@ public class MonsterBullet : MonoBehaviour
         InvokeRepeating("ShootBullet", startDelay, endDelay);
     }
 
-    void makeBullet()
+   
+   
+   void ShootBullet()
     {
         float x = _monsterTransform.position.x;
         float y = _monsterTransform.position.y - 2.0f;
-        Instantiate(Bullet, new Vector3(x, y, 0), Quaternion.identity);
-    }
-
-   void BulletVector()
-    {
+        
         Vector3 direction = (_playerTransform.position - _monsterTransform.position).normalized;
         float speed = monsterBulletData.speed;
 
         GameObject bullet = Instantiate(Bullet, _monsterTransform.position, Quaternion.identity);
         bullet.GetComponent<Rigidbody2D>().velocity = direction * speed;
     }
-
-    void ShootBullet()
-    {
-        makeBullet();
-        BulletVector();
-    }
-
- 
-
 
 
 }
