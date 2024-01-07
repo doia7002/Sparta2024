b35b513e33bb61f15780ced8ead875c33b43a3a7
ptt0313 <149345231+ptt0313@users.noreply.github.com>
2024-01-07 15:07:25 +0900

    [feat]BossPattern

---
+++ Assets/ObjectPools/ObjectManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    //public GameObject bossBulletPrefab;
+    //GameObject[] bossBullet;
+
+    //private void Awake()
+    //{
+    //    bossBullet = new GameObject[30];
+
+    //    Generate();
+    //}
+
+    //void Generate()
+    //{
+    //    for(int index=0; index < bossBullet.Length;index++)
+    //    {
+    //        bossBullet[index] = Instantiate(bossBulletPrefab);
+    //        bossBullet[index].SetActive(false);
+    //    }
+    //}
+
+    //public GameObject Makebullet()
+    //{
+
+    //    for(int index=0; index<bossBullet.Length;index++)
+    //    {
+    //        if(!bossBullet[index].activeSelf)
+    //        {
+    //            bossBullet[index].SetActive(true);
+    //            return bossBullet[index];
+    //        }
+    //    }
+    //    return null;
+    //}
+}
